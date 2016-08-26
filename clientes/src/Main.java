d9b32a566c8530f6e799290415098b5f79abbdd6
cheoantonio0525 <cheoantonio0525@live.com>
2016-08-26 09:36:41 -0500

    Update Main.java
    prueba

--- clientes/src/Main.java
+++ clientes/src/Main.java
@@ -14,6 +14,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        string v=0;
+        sout;
         // TODO code application logic here
     }
     
